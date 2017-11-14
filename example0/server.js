da76efd151db33f93e5cb59ad2a2eefa8a9854f1
Raymond So <rso@ouhk.edu.hk>
2017-11-14 14:18:05 +0800

    serve-static middleware

---
+++ example0/server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+// Middleware that serves static files in the public folder
+// GET /index.html
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 8099);
