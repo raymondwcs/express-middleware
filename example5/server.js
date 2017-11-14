6a536debe5e99f9ad98c0ed0cf2eb99a1da045ec
Raymond So <rso@ouhk.edu.hk>
2017-11-14 14:15:02 +0800

    fixed minor bugs

--- example5/server.js
+++ example5/server.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 // Middleware that serves static files in the public folder
 // GET /index.html
 app.use(express.static('public'));
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.post('/login',function(req,res,next) {
    res.write('ID = ' + req.body.id);
