ae82fbe5193072946fd1fb0a9efb963801a3ff9d
Raymond So <rso@ouhk.edu.hk>
2017-11-14 12:46:56 +0800

    logger middleware

---
+++ example1/server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.use(function(req,res,next) {
+  console.log(req.method + ' ' + req.url +
+              ' was requested at ' + Date.now());
+  next();
+})
+
+app.get('/', function(req, res, next) {
+  res.send('Hello World!');
+});
+
+app.get('/help', function(req, res, next) {
+  res.send('Nope.. nothing to see here');
+});
+
+app.listen(process.env.PORT || 8099);
