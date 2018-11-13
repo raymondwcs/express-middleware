fe46361e84bb75e7044b24911aa34374f696384d
Raymond SO <rso@ouhk.edu.hk>
2018-11-14 07:50:13 +0800

    Update server.js

--- example0/server.js
+++ example0/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-// Middleware that serves static files in the public folder
+// Middleware that serves static files in the 'public' folder
 // GET /index.html
 app.use(express.static('public'));
 
